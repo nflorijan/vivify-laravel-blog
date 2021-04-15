db3993ab3369163144f7211dc76cc2e410a2cdf9
nflorijan <florijan.nadj@gmail.com>
2021-04-15 21:46:15 +0200

    adding is published:

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::published()->get();
         return view('posts.index', compact('posts'));
     }
 
     public function show(Post $post) {
+        if(!$post->is_published) {
+           throw new ModelNotFoundException();
+        }
         return view('posts.show', compact('post'));
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,14 @@ class Post extends Model
 {
     use HasFactory;
 
+    public static function published() {
+        return self::where('is_published', true);
+    }
+
+    public static function unpublished() {
+        return self::where('is_published', false);
+    }
+
     public function getRandomNumber() {
         return 4;
     }

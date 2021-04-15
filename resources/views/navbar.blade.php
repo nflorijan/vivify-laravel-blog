65021a4a8f73e3a07ce7c339c935aab8b836715e
nflorijan <florijan.nadj@gmail.com>
2021-04-15 21:59:25 +0200

    adding simple navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,13 +1,7 @@
-<!-- Image and text -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/posts">Vivify Blog</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/posts">
+      Vivify Blog
+    </a>
   </div>
 </nav>

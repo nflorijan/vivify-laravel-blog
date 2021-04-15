488dfc0167c9923575aaca3b14b9b6910dd3b0d8
nflorijan <florijan.nadj@gmail.com>
2021-04-15 21:55:15 +0200

    Adding navbar template

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,13 @@
+<!-- Image and text -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/posts">Vivify Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,6 @@
 @section('title', $post->title)
 
 @section('content')
-  <a href="/posts">Back to posts</a>
   <h2>{{ $post->title }}</h2>
   <p>{{ $post->body }}</p>
 @endsection

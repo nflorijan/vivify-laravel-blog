3cb8d7ec38964b2f868849c9aa8d30b75c2194c5
nflorijan <florijan.nadj@gmail.com>
2021-04-27 19:04:12 +0200

    Adding User and Post Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      info('Seeding users');
+      $this->call([
+          UserSeeder::class,
+          PostSeeder::class
+      ]);
     }
 }
